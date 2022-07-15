272d33ac5a31d8c5eb641590092293ce933cab2c
SalmanAsh <102732552+SalmanAsh@users.noreply.github.com>
2022-07-15 19:27:46 +0100

    beginning

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+print("Welcome to my first game!")
+name = input("What is your name? ")
+age = int(input("What is your age? "))
 
+print(f"\nHello {name.title()}! You are {age}.")
+
+if age >= 18:
+    print("You can play this game!")
+    want_to_play = input("Do you want to play?")
+    if want_to_play == "yes":
+        print("Let's play!")
+elif age >= 14:
+    print("You can play under supervision!")
+else:
+    print("You must be at least 18 to play this game!")
