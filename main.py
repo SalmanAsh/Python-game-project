ebd4753456607a8aab984879fe625bc708de7667
SalmanAsh <102732552+SalmanAsh@users.noreply.github.com>
2022-07-15 20:12:52 +0100

    completed

--- main.py
+++ main.py
@@ -4,11 +4,47 @@ age = int(input("What is your age? "))
 
 print(f"\nHello {name.title()}! You are {age}.")
 
+health = 10
 if age >= 18:
-    print("You can play this game!")
-    want_to_play = input("Do you want to play?")
-    if want_to_play == "yes":
-        print("Let's play!")
+    print("\tYou can play this game!")
+    want_to_play = input("Do you want to play? ")
+    if want_to_play.lower() == "yes":
+        print("\nLet's play! ")
+        print(f"Your current health is {health}!")
+
+        left_or_right = input("First choice... Left ot Right(left/right)? ")
+        if left_or_right.lower() == 'right':
+            ans = input(
+                "Nice, you follow the path and reach a lake.. Do you swim across or go around (across/around? ")
+            if ans.lower() == 'around':
+                print("You went around and reached the other side of the lake")
+            elif ans.lower() == 'across':
+                print(
+                    "\nYou managed to to get across but were bit by a fish and lost 5 health")
+                health -= 5
+                print(f"Your current health is {health}.")
+            else:
+                print("\nYou lost!")
+
+            ans = input(
+                "You notice a house and a river. Which one you go to (house/river)? ")
+            if ans.lower() == 'house':
+                print(
+                    "You want to the house and got beaten by the owner.  You lost 5 health.")
+                health -= 5
+                if health <= 0:
+                    print("You lost!")
+                else:
+                    print("\nYou have made it!")
+                    print("Well Done!")
+            else:
+                print("\nYou fell in the river.")
+                print("You lost!")
+        else:
+            print("\nYou fell down and lost...")
+
+    else:
+        print("Bye Bye!")
 elif age >= 14:
     print("You can play under supervision!")
 else:
